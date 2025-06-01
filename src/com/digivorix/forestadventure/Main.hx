b9e241f5da0f8ad8a1dc95bf8aef2473acfb2325
Toby Davis <tobias@digivorix.com>
2025-06-01 15:13:53 -0400

    Initial project files commit

---
+++ src/com/digivorix/forestadventure/Main.hx
@@ -0,0 +1,21 @@
+package com.digivorix.forestadventure;
+
+import openfl.display.Sprite;
+import openfl.Lib;
+
+/**
+ * ...
+ * @author Toby Davis
+ */
+class Main extends Sprite 
+{
+
+	public function new() 
+	{
+		super();
+		
+		// Assets:
+		// openfl.Assets.getBitmapData("img/assetname.jpg");
+	}
+
+}
