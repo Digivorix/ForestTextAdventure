652507ca5648a5f4183aa571e197777f0f46e1d5
Toby Davis <tobias@digivorix.com>
2025-06-04 16:19:12 -0400

    Finish most of the game loop
    Fix inputs so that they can only be typed after the text display has finished updating.
    
    Add more comments.
    
    Finish story.

--- src/com/digivorix/forestadventure/Main.hx
+++ src/com/digivorix/forestadventure/Main.hx
@@ -21,27 +21,27 @@ class Main extends Sprite
 	
 	var cacheTime:Int; // Stored at the end of each frame
 	var deltaTime:Int; // Current delta time
-	var pause:Int = 0; // Update loop pause length (multiplied by 1000)
-	var storyPos:Int = 0;
+	var pause:Float = 0; // Update loop pause length (multiplied by 1000)
+	var storyPos:Int = 0; // Current story position (used in update loop and input checks)
 	
 	// Variables for updating the text list
 	var linesToPush:Array<String>;
 	var linesPushed:Int = 0;
 	
 	var running:Bool = true; // Is application running?
-	var cont:Bool = true;
+	var cont:Bool = true; // Continue with story in the update loop?
 	var userQuit:Bool = false; // Did user agree to the game?
 	var gameComplete:Bool = false; // Has the game been completed?
-	var gameLost:Bool = false;
-	var waitingForInput:Bool = false;
+	var gameLost:Bool = false; // Has the game been lost?
+	var waitingForInput:Bool = false; // Is the game waiting for the user's input?
 	
 	var keys:Array<Bool>; // Store the value of keys pressed
 	var allowInput:Bool = false; // Is input allowed?
 	
 	// Sprite containers for various elements
-	var graphicSprite:Sprite;
-	var listSprite:Sprite;
-	var inputSprite:Sprite;
+	var graphicSprite:Sprite; // Contains the story graphics
+	var listSprite:Sprite; // Contains the text list (aka the terminal readout)
+	var inputSprite:Sprite; // Contains the input text box
 	
 	// Font format
 	var terminalFormat:TextFormat;
@@ -49,7 +49,7 @@ class Main extends Sprite
 	
 	// List text variables
 	var listTextField:TextField;
-	var textList:Array<String>;
+	var textList:Array<String>; // List of text that is looped through and displayed in the listTextField
 	
 	// Input text variable
 	var inputTextField:TextField;
@@ -74,15 +74,8 @@ class Main extends Sprite
 		addChild(graphicSprite);
 		
 		// Set up terminal text format
-		#if html5
-		terminalFormat = new TextFormat("Perfect DOS VGA 437", 16, 0xFFFFFF); // HTML5 weirdness
-		#end
-		#if cpp
-		terminalFormat = new TextFormat("Perfect DOS VGA 437", 20, 0xFFFFFF);
-		#end
-		#if (!html5 && !cpp)
-		terminalFormat = new TextFormat("Perfect DOS VGA 437", 18, 0xFFFFFF);
-		#end
+		terminalFormat = new TextFormat("Perfect DOS VGA 437", 16, 0xFFFFFF);
+		
 		// Set up input text format
 		inputFormat = new TextFormat("Perfect DOS VGA 437", 24, 0xFFFFFF);
 		
@@ -150,6 +143,7 @@ class Main extends Sprite
 		#end
 	}
 	
+	// Restart the game
 	private function restart(pos:Int):Void{
 		//trace("Restart game");
 		pause = 0;
@@ -201,6 +195,7 @@ class Main extends Sprite
 			restart(4);
 		}
 		
+		// Submit or subtract from input box content
 		if (allowInput){
 			if(checkKey(Keyboard.BACKSPACE)){
 				inputTextField.text = inputTextField.text.substring(0, inputTextField.length-1);
@@ -212,6 +207,7 @@ class Main extends Sprite
 		}
 		
 		if(cont && !waitingForInput){ // Are we allowed to continue and not waiting for user input?
+			allowInput = false;
 			switch(storyPos){
 				case 0:
 					wait(1);
@@ -224,15 +220,15 @@ class Main extends Sprite
 					"Initializing Gaming Electronic Operating System Version 1.0",
 					"Copyright (c) 2017-2025 Toby Davis",
 					"", 
-					"Hello", 
+					"<SYSTEM> Hello", 
 					"", 
-					"I am GEOS"
+					"<SYSTEM> I am GEOS"
 					];
 					storyPos++;
 				case 2:
 					resetPushedLines();
-					linesToPush = ["", "Would you like to play a game? (Y | N)"];
-					allowInput = true;
+					linesToPush = ["", "<SYSTEM> Would you like to play a game? (Y | N)"];
+					//allowInput = true;
 					waitingForInput = true;
 				case 3:
 					resetPushedLines();
@@ -252,7 +248,7 @@ class Main extends Sprite
 					"You start out on a dirt road in a forest with a fork in it.",
 					"Do you go left or right? (LEFT | RIGHT)"
 					];
-					allowInput = true;
+					//allowInput = true;
 					waitingForInput = true;
 				case 5:
 					resetPushedLines();
@@ -261,7 +257,7 @@ class Main extends Sprite
 					"You then come to a pool of lava.",
 					"Do swim across or take a boat? (SWIM | BOAT)"
 					];
-					allowInput = true;
+					//allowInput = true;
 					waitingForInput = true;
 				case 6:
 					resetPushedLines();
@@ -270,7 +266,7 @@ class Main extends Sprite
 					"Later you find an apple and a banana, you are hungry.",
 					"Which one do you eat? (APPLE | BANANA)"
 					];
-					allowInput = true;
+					//allowInput = true;
 					waitingForInput = true;
 				case 7:
 					resetPushedLines();
@@ -280,24 +276,63 @@ class Main extends Sprite
 					"You also see a pack of dynamite.",
 					"Which do you choose? (SWORD | DYNAMITE)"
 					];
-					allowInput = true;
+					//allowInput = true;
 					waitingForInput = true;
+				case 8:
+					resetPushedLines();
+					linesToPush = [
+					"",
+					"You find a electric fence blocking your path.",
+					"Do you blow it up with a stick of dynamite?", "Or run as fast as you can at it?",
+					"(DYNAMITE | RUN)"
+					];
+					//allowInput = true;
+					waitingForInput = true;
+				case 9:
+					resetPushedLines();
+					linesToPush = [
+					"",
+					"You come to another clearing and see a book and a strange looking device.",
+					"Do you pick up the book or the device? (BOOK | DEVICE)"
+					];
+					//allowInput = true;
+					waitingForInput = true;
+				case 10:
+					resetPushedLines();
+					linesToPush = [
+					"",
+					"You find a nuke and its detonator and a brick after entering the castle.",
+					"Which one do you take, the nuke or brick? (NUKE | BRICK)"
+					];
+					//allowInput = true;
+					waitingForInput = true;
+				case 11:
+					resetPushedLines();
+					linesToPush = [
+					"",
+					"You have found a dragon upstairs! You only get one shot!",
+					"Do you use the nuke or all of the sticks of dynamite? (NUKE | DYNAMITE)"
+					];
+					//allowInput = true;
+					waitingForInput = true;
+				
 			}
 			cont = false;
 		}
-		else{
-			if(linesPushed < linesToPush.length){
-				progressStory(1);
+		else{ // For after a new prompt is pushed to the display
+			if(linesPushed < linesToPush.length){ //Will add the new lines to the display before anything else
+				progressStory(0.5);
 				//wait(2);
 			}
 			else if(waitingForInput){
 				// Waiting
+				if (!allowInput)
+					allowInput = true;
 			}
 			else if(!(linesPushed < linesToPush.length) && !waitingForInput && !userQuit && !gameLost && !gameComplete){
-				// Resume if there are no lines waiting for a push, no pause for a user input, and the user has not quit the game
+				// Resume if there are no lines waiting for a push, no pause for a user input, and the game has not ended in any way
 				cont = true;
 			}
-			//checkCurrentPosState(); ???
 		}
 	}
 	
@@ -309,12 +344,14 @@ class Main extends Sprite
 		return false;
 	}
 	
+	// Reset the pushed line variables
 	private function resetPushedLines():Void{
 		linesToPush = [];
 		linesPushed = 0;
 	}
 	
-	private function progressStory(delay:Int = 0):Void{
+	// Progress the story by pushing the new lines to the display
+	private function progressStory(delay:Float = 0):Void{
 		//trace("Progress story");
 		var newLine:String = linesToPush[linesPushed];
 		
@@ -326,6 +363,7 @@ class Main extends Sprite
 		wait(delay); // Set pause for next loop
 	}
 	
+	// Decide what will happen based on the user's input
 	private function processInput():Void{
 		var input:String = inputTextField.text;
 		inputTextField.text = "";
@@ -334,7 +372,7 @@ class Main extends Sprite
 			case 2: // Confirm start
 				if (input == "Y"){
 					resetPushedLines();
-					linesToPush = ["", "Great, I like games."];
+					linesToPush = ["", "<SYSTEM> Great, I like games."];
 					storyPos++;
 					waitingForInput = false;
 				}
@@ -343,11 +381,11 @@ class Main extends Sprite
 					trace("User has quit game");
 					#if (!flash && !html5)
 					// Native
-					linesToPush = ["", "Not now? Okay", "", "Goodbye2"];
+					linesToPush = ["", "<SYSTEM> Not now? Okay", "", "<SYSTEM> Goodbye"];
 					userQuit = true;
 					#else
 					// Flash and Web
-					linesToPush = ["", "Not now? Okay", "", "Goodbye3", "", "Press R to Restart"];
+					linesToPush = ["", "<SYSTEM> Not now? Okay", "", "<SYSTEM> Goodbye", "", "Press R to Restart"];
 					gameLost = true;
 					#end
 				}
@@ -424,13 +462,78 @@ class Main extends Sprite
 					linesToPush = ["", "<INVALID INPUT>"];
 					waitingForInput = false;
 				}
+			case 8: // Fence
+				if (input == "DYNAMITE"){
+					resetPushedLines();
+					linesToPush = ["", "You don't know how to use dynamite properly yet!", "You blew yourself up!", "Your score is 4", "", "Press R to Restart"];
+					gameLost = true;
+				}
+				else if (input == "RUN"){
+					resetPushedLines();
+					linesToPush = ["", "The fence was a hologram!", "It was too expensive to get a real one,", "so a hologram was put in place to scare away intruders.", "You cross through it safely.", "You continue to follow the dirt road."];
+					storyPos++;
+					waitingForInput = false;
+				}
+				else{
+					resetPushedLines();
+					linesToPush = ["", "<INVALID INPUT>"];
+					waitingForInput = false;
+				}
+			case 9: // Strange device and book
+				if (input == "DEVICE"){
+					resetPushedLines();
+					linesToPush = ["", "The device was a black hole generator and you turned it on!", "Well, there goes the Earth, see ya!", "Your score is 5", "", "Press R to Restart"];
+					gameLost = true;
+				}
+				else if (input == "BOOK"){
+					resetPushedLines();
+					linesToPush = ["", "You read the book! You now know how to use dynamite.", "You're lucky you didn't try to use it before or you could have blown yourself up!", "You see a castle up ahead and decide to go and enter it"];
+					storyPos++;
+					waitingForInput = false;
+				}
+				else{
+					resetPushedLines();
+					linesToPush = ["", "<INVALID INPUT>"];
+					waitingForInput = false;
+				}
+			case 10: // Nuke and brick
+				if (input == "BRICK"){
+					resetPushedLines();
+					linesToPush = ["", "The brick was part of a crucial support system!", "You're now buried under a pile of rubble! You're also dead.", "Your score is 6", "", "Press R to Restart"];
+					gameLost = true;
+				}
+				else if (input == "NUKE"){
+					resetPushedLines();
+					linesToPush = ["", "Hey, you didn't blow up! You now have a nuke and its detonator!", "You continue upstairs to where you just heard a loud ROAR!"];
+					storyPos++;
+					waitingForInput = false;
+				}
+				else{
+					resetPushedLines();
+					linesToPush = ["", "<INVALID INPUT>"];
+					waitingForInput = false;
+				}
+			case 11: // Dragon
+				if (input == "NUKE"){
+					resetPushedLines();
+					linesToPush = ["", "You blew up the dragon, but also yourself, the castle, and the surrounding area!", "Good job hero.", "Your score is 7/8. You were so close!", "", "Press R to Restart"];
+					gameLost = true;
+				}
+				else if (input == "DYNAMITE"){
+					resetPushedLines();
+					linesToPush = ["", "You blew up the dragon and lived!", "You return to your town a hero and live you life to a nice old age.", "Your score is 8/8. YOU WIN!"];
+					storyPos++;
+					waitingForInput = false;
+				}
+				else{
+					resetPushedLines();
+					linesToPush = ["", "<INVALID INPUT>"];
+					waitingForInput = false;
+				}
 		}
 	}
 	
-	private function isValid():Bool{
-		return false;
-	}
-	
+	//Add a new line of text to display
 	private function printList(text:String, delay:Int = 0):Void{
 		textList.push(text);
 		updateList();
@@ -438,16 +541,24 @@ class Main extends Sprite
 	
 	// Update the displayed text list
 	private function updateList(delay:Int = 0):Void{
+		var maxLines:Int = 12;
+		
+		#if (!html5)
+		maxLines = 15;
+		#else
+		maxLines = 12; // Less lines to account for HTML5 text weirdness
+		#end
+		
 		var newText:String = "";
 		
 		var lines:Int = 0;
 		var startPos:Int = 0;
 		
-		if(textList.length <= 12){
+		if(textList.length <= maxLines){
 			startPos = 0;
 		}
 		else{
-			startPos = textList.length - 12;
+			startPos = textList.length - maxLines;
 		}
 		
 		while(startPos < textList.length){
@@ -461,8 +572,8 @@ class Main extends Sprite
 	}
 	
 	// Pause the next update loop execution for the specified time (in seconds)
-	private function wait(length:Int):Void{
-		//pause = length * 1000;
+	private function wait(length:Float):Void{
+		pause = length * 1000;
 	}
 	
 	// Final actions before application ends
@@ -514,7 +625,7 @@ class Main extends Sprite
 		cacheTime = currentTime; // Update the cached time
 	}
 	
-	private function onResize(event:Event):Void {
+	private function onResize(event:Event):Void { // For native platforms
 		#if (!flash && !html5)
 		var multX:Float = stage.stageWidth / 800; 
 		var multY:Float = stage.stageHeight / 480;
